17e1a67e5bcaeace17b5c1197a15087cf5955001
vinay chaudhary <97957688+vinayofc@users.noreply.github.com>
2022-06-12 14:40:21 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,42 @@
+const script1 = document.createElement('script');
+const script2 = document.createElement('script');
+const script3 = document.createElement('script');
+const script4 = document.createElement('script');
+const style1 = document.createElement('style');
+const style2 = document.createElement('style');
+
+
+script.setAttribute(
+  'src',
+  'https://vinayofc.github.io/cdn/plugin/components/jquery/jquery-1.9.0.min.js',
+);
+
+script2.setAttribute(
+  'src',
+  'https://vinayofc.github.io/cdn/plugin/components/moment/moment.min.js',
+);
+
+script3.setAttribute(
+  'src',
+  'https://vinayofc.github.io/cdn/plugin/components/moment/moment-timezone-with-data.min.js',
+);
+
+script4.setAttribute(
+  'src',
+  'https://vinayofc.github.io/cdn/plugin/czm-chat-support.min.js',
+);
+
+style1.setAttribute(
+  'href',
+  'https://vinayofc.github.io/cdn/plugin/components/font-awesome/css/fontawesome.min.css',
+  'rel',
+  'stylesheet',
+);
+
+
+style2.setAttribute(
+  'href',
+  'https://vinayofc.github.io/cdn/plugin/czm-chat-support.css',
+  'rel',
+  'stylesheet',
+);
